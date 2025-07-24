eb063bc5767acb11265c3ac1f5fb68ef4674d2f7
SaiPranab <saipranabpatra1234@gmail.com>
2025-07-24 08:11:26 +0530

    debugger in js

---
+++ day 12 debugger, varables memory address/script.js
@@ -0,0 +1,14 @@
+// debugger
+// console.log(firstName)
+// console.log(lastName)
+// console.log(age)
+
+var firstName = "Akash"
+let lastName = "Singh"
+const age = 25
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+
+let a = "xyz"
