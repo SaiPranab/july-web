34d3c2cb9470bbf4370c0768b5141a69607a6a7b
SaiPranab <saipranabpatra1234@gmail.com>
2025-07-24 07:06:20 +0530

    variables in js

---
+++ day 11 data types in js/variables in js.js
@@ -0,0 +1,70 @@
+let username = 'Akash'
+console.log(username)
+console.log(typeof username)
+
+let age = 25
+console.log(age)
+console.log(typeof age)
+
+let isHappy = true
+console.log(isHappy)
+console.log(typeof isHappy)
+
+let lastName = 'Singh'
+console.log(lastName)
+console.log(typeof lastName)
+
+username = 'Sipun'
+username = 100
+console.log(username)
+console.log(typeof username)
+
+let userIntro = "Hii I am " + username + " " + lastName + ", I am " + age + " years old"
+console.log(userIntro)
+
+let a = undefined
+console.log(a)
+console.log(typeof a)
+
+let b;
+console.log(b)
+console.log(typeof b)
+
+b = false
+console.log(b, typeof b, 'xyz')
+
+let c = null
+console.log(c, typeof c)
+
+
+// <==========================================================================>
+const hoursInADay = 24
+// hoursInADay = 20
+// hoursInADay = "JT"
+
+// const d;
+// ===================================================================
+var e
+e = 10;
+console.log(e)
+
+var f;
+console.log(f)
+
+// var g = 1120
+// {
+//     var g = 200
+//     let h = 30;
+// }
+
+// console.log(g)
+// console.log(h)
+
+// console.log(x) //  referenceError -> caanot access x before initlisation
+let x = 2020;
+console.log(x) // 2020
+
+
+console.log('before initiliasation the value opf y', y) // undefined
+var y = 3020
+console.log(y)
