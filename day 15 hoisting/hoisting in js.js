0c4d4fd7e8b41fbeca732ee71558ff100f43d1cb
SaiPranab <saipranabpatra1234@gmail.com>
2025-07-27 07:42:27 +0530

    hoisting in javascript

---
+++ day 15 hoisting/hoisting in js.js
@@ -0,0 +1,13 @@
+console.log(username)
+var username = "Abhishek"
+console.log(username)
+
+
+// console.log(lastname)
+// const lastname = "Das"
+
+
+sayHii()
+function sayHii() {
+    console.log("Hello everyone!!!")
+}
