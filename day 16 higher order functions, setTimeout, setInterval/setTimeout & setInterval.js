8e052346500a7cbbf2eb6329ea33c0a28a99861b
SaiPranab <saipranabpatra1234@gmail.com>
2025-07-29 07:46:03 +0530

    setTimeout & setInterval

---
+++ day 16 higher order functions, setTimeout, setInterval/setTimeout & setInterval.js
@@ -0,0 +1,14 @@
+console.log(1)
+
+setTimeout(hello, 5000)
+
+function hello() {
+    console.log("hello everyone!!!")
+}
+
+console.log(3)
+
+// const id = setInterval(hello, 2000)
+// console.log(id)
+
+console.log(setInterval(hello, 1000))
