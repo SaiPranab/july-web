bd8c75b77641efc74a1e133acc24418be36c4058
SaiPranab <saipranabpatra1234@gmail.com>
2025-08-01 07:14:32 +0530

    how to delete, seal & freeze objects

--- day 17 objects & methods/objects2.js
+++ day 17 objects & methods/objects2.js
@@ -13,4 +13,20 @@ const person = {
 // person.lastName = "Sahoo"
 // console.log(person.lastName)
 
-// perosn = {} // error -> as person is declared with const
+// pesosn = {} // error -> as person is declared with const
+
+
+// let person2 = {
+//     firstName: 'Raj',
+//     lastName: 'Patra'
+// }
+// person2 = {} // empty object
+
+
+// const isDeleted = delete person.lastName
+// console.log(isDeleted)
+// console.log(person)
+
+// Object.seal(person) // it restrict the Object, so that any addition & deletion of any key can't be possible (still updating key & values are possible)
+
+Object.freeze(person) // through freeze method, we can't make any kind of operation (except extracting values) inside the object
