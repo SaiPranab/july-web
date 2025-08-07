0090e28060a3ae7abf8bb789bd85947800431bfd
SaiPranab <saipranabpatra1234@gmail.com>
2025-08-07 08:00:42 +0530

    bom, intro to dom

---
+++ day 23 DOM/1. script.js
@@ -0,0 +1,7 @@
+document.body.children[0].innerText = '<i>Sipun</i>'
+
+document.body.children[1].innerHTML = '<i>Patra</i>'
+
+
+console.log(document.body.children[1].innerHTML)
+console.log(document.body.children[1].innerText)
