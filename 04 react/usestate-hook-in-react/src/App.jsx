3e4f803a764826f23a76e085950c8ecc5321ec1d
Sai <saipranabpatra5233@gmail.com>
2025-09-08 07:28:05 +0530

    advance useState hook

--- 04 react/usestate-hook-in-react/src/App.jsx
+++ 04 react/usestate-hook-in-react/src/App.jsx
@@ -3,13 +3,16 @@ import './App.css'
 
 let count = 0;
 
-// const data = useState(0) // useState(initialValue)
+// const data = useState(0) // [state, setState] = useState(initialValue)
+
+console.log("Hellloooooo")
 
 function App({ root }) {
   // const data = useState(100)
   // console.log('data:-', data)
-
+  
   const [counter, setCounter] = useState(100)
+  console.log("rendering", counter)
 
   const increaseValue = (e) => {
     console.log("Increase button clicked")
@@ -25,7 +28,37 @@ function App({ root }) {
     // setData(data[0]++)
     // console.log(data[0])
 
-    setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(100)
+    // console.log("Counter Value :-", counter)
+
+    // console.log("Counter Value ", counter)
+    // setCounter(counter + 1) // counter = 100, setCounter = 100 + 1 => 101
+    // console.log("Counter Value ", counter)
+    // setCounter(counter + 1) // counter = 100, setCounter = 100 + 1 => 101
+    // console.log("Counter Value ", counter)
+    // setCounter(counter + 1) // counter = 100, setCounter = 100 + 1 => 101 
+    // console.log("Counter Value ", counter)
+
+    // setCounter((prevState) => {
+    //   console.log("1st Previous State is ", prevState)
+    //   console.log("1st Counter Value ", counter)
+    //   return prevState + 1; 
+    // })
+    // setCounter((prevState) => {
+    //   console.log("2nd Previous State is ", prevState) 
+    //   console.log("2nd Counter Value ", counter)
+    //   return prevState + 1; 
+    // })
+    // setCounter((prevState) => {
+    //   console.log("3rd Previous State is ", prevState) 
+    //   console.log("3rd Counter Value ", counter)
+    //   return prevState + 1; 
+    // })
+
+    setCounter(prevState => prevState + 1)
+    setCounter(prevState => prevState + 1)
+    setCounter(prevState => prevState + 1)
   }
 
   const decreaseValue = (e) => {
