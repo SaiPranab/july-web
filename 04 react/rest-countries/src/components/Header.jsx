289e8dbf8d6704490ea16363e10a813a9ac371b3
SaiPranab <saipranabpatra1234@gmail.com>
2025-09-20 08:00:06 +0530

    Dark mode & white mode toggle

--- 04 react/rest-countries/src/components/Header.jsx
+++ 04 react/rest-countries/src/components/Header.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react"
+
 export default function Header() {
+  const [isDark, setIsDark] = useState(localStorage.getItem("isDarkMode"))
+
+  if (isDark) {
+    document.body.classList.add('dark')
+    localStorage.setItem("isDarkMode", true)
+  } else {
+    document.body.classList.remove('dark')
+    localStorage.setItem("isDarkMode", false)
+  }
+
   return (
     <header className="header-container">
       <div className="header-content">
         <h2 className="title">
           <a href="/">Where in the world?</a>
         </h2>
-        <p className="theme-changer">
-          <i className="fa-regular fa-moon" />
-          &nbsp;&nbsp;Dark Mode
+        <p className="theme-changer" onClick={() => setIsDark(!isDark)}>
+          <i className={isDark ? "fa-solid fa-sun" : "fa-regular fa-moon"} />
+          &nbsp;&nbsp;{isDark ? 'Light' : 'Dark'} Mode
         </p>
       </div>
     </header>
