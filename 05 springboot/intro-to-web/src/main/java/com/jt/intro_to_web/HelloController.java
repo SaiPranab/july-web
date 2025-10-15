bc37dc83d8e38979a2caa1431ba12868273168ba
SaiPranab <saipranabpatra1234@gmail.com>
2025-10-15 20:59:04 +0530

    thymeleaf and spring  devtool

--- 05 springboot/intro-to-web/src/main/java/com/jt/intro_to_web/HelloController.java
+++ 05 springboot/intro-to-web/src/main/java/com/jt/intro_to_web/HelloController.java
@@ -4,8 +4,8 @@ import java.io.PrintWriter;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
 public class HelloController {
@@ -14,5 +14,18 @@ public class HelloController {
   public void sayHello(PrintWriter writer) {
     System.out.println("Hello To Web");
     writer.println("Hello To Web In Browser");
-  }  
+  }
+
+  @RequestMapping({ "/", "/home" })
+  public String home() {
+    System.out.println("Home method1");
+    return "index";
+  }
+
+  @RequestMapping("/contact")
+  public String contact(Model model) {
+    model.addAttribute("name1", "Java Technocrat");
+    model.addAttribute("phone", "9437056780");
+    return "contact";
+  }
 }
