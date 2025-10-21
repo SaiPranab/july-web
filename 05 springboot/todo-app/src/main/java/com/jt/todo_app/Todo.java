e6d791321b2e68b9e025f9f877cb47a3627a323c
SaiPranab <saipranabpatra1234@gmail.com>
2025-10-21 20:57:41 +0530

    add: Todo model to effecently create & manage todos feat: create todo refactror: todos list to store todo object instead of simple task

---
+++ 05 springboot/todo-app/src/main/java/com/jt/todo_app/Todo.java
@@ -0,0 +1,32 @@
+package com.jt.todo_app;
+
+public class Todo {
+  private int id;
+  private String task;
+  private boolean completed;
+
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public String getTask() {
+    return task;
+  }
+  public void setTask(String task) {
+    this.task = task;
+  }
+  public boolean isCompleted() {
+    return completed;
+  }
+  public void setCompleted(boolean completed) {
+    this.completed = completed;
+  }
+
+  public Todo(int id, String task, boolean completed) {
+    this.id = id;
+    this.task = task;
+    this.completed = completed;
+  }
+}
--- 05 springboot/todo-app/src/main/java/com/jt/todo_app/TodoController.java
+++ 05 springboot/todo-app/src/main/java/com/jt/todo_app/TodoController.java
@@ -1,16 +1,34 @@
 package com.jt.todo_app;
 
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
+  private final List<Todo> todos;
+
+  public TodoController(List<Todo> todos) {
+    this.todos = todos;
+  }
+
   @GetMapping
   public String home(Model model) {
-    model.addAttribute("todos", List.of("Eat", "Sleep", "Code", "Repeat"));
+    model.addAttribute("todos", todos);
     return "index";
   }
+
+  @PostMapping("/add")
+  public String addTodo(@RequestParam String task) {
+    if (task != null && !task.isEmpty() && !task.isBlank()) {
+      Todo todo = new Todo(0, task, false);
+      todos.add(todo);
+    }
+
+    return "redirect:/";
+  }
+
 }
