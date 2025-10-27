bb28d63b77d20d652b365ed3790e00b0d6856a14
SaiPranab <saipranabpatra1234@gmail.com>
2025-10-27 20:05:12 +0530

    chore: merge conflict

--- 05 springboot/jt-blogs/src/main/java/com/jt/jt_blogs/controller/BlogController.java
+++ 05 springboot/jt-blogs/src/main/java/com/jt/jt_blogs/controller/BlogController.java
@@ -1,4 +1,19 @@
 package com.jt.jt_blogs.controller;
 
+import com.jt.jt_blogs.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class BlogController {
+    private final BlogService service;
+
+    @GetMapping
+    public String home(Model model) {
+        model.addAttribute("blogs", service.getBlogs());
+        return "home";
+    }
 }
--- 05 springboot/jt-blogs/src/main/java/com/jt/jt_blogs/service/BlogService.java
+++ 05 springboot/jt-blogs/src/main/java/com/jt/jt_blogs/service/BlogService.java
@@ -1,4 +1,18 @@
 package com.jt.jt_blogs.service;
 
+import com.jt.jt_blogs.model.Blog;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BlogService {
+
+    public List<Blog> getBlogs() {
+        Blog blog1 = new Blog(1, "Heading 1", "Description 1");
+        Blog blog2 = new Blog(1, "Heading 2", "Description 2");
+        Blog blog3 = new Blog(1, "Heading 3", "Description 3");
+
+        return List.of(blog1, blog2, blog3);
+    }
 }
