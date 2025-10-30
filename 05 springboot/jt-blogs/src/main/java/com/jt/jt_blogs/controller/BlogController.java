882592aeafae71350bb63edd7901a06edcdb58ce
SaiPranab <saipranabpatra1234@gmail.com>
2025-10-30 21:08:18 +0530

    add: implemented JdbcTemplate for creating a blog using Spring JDBC

--- 05 springboot/jt-blogs/src/main/java/com/jt/jt_blogs/controller/BlogController.java
+++ 05 springboot/jt-blogs/src/main/java/com/jt/jt_blogs/controller/BlogController.java
@@ -5,15 +5,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class BlogController {
-    private final BlogService service;
+  private final BlogService service;
 
-    @GetMapping
-    public String home(Model model) {
-        model.addAttribute("blogs", service.getBlogs());
-        return "home";
-    }
+  @GetMapping
+  public String home(Model model) {
+    model.addAttribute("blogs", service.getBlogs());
+    return "home";
+  }
+
+  @GetMapping("/form")
+  public String form() {
+    return "add-blog";
+  }
+
+  @PostMapping("/add-blog")
+  public String addBlog(@RequestParam String heading, @RequestParam String description) {
+    service.createBlog(heading, description);
+    return "redirect:/";
+  }
 }
--- 05 springboot/jt-blogs/src/main/java/com/jt/jt_blogs/service/BlogService.java
+++ 05 springboot/jt-blogs/src/main/java/com/jt/jt_blogs/service/BlogService.java
@@ -1,12 +1,18 @@
 package com.jt.jt_blogs.service;
 
 import com.jt.jt_blogs.model.Blog;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class BlogService {
+    private final JdbcTemplate jdbcTemplate;
 
     public List<Blog> getBlogs() {
         Blog blog1 = new Blog(1, "Heading 1", "Description 1");
@@ -14,5 +20,10 @@ public class BlogService {
         Blog blog3 = new Blog(1, "Heading 3", "Description 3");
 
         return List.of(blog1, blog2, blog3);
+    }
+
+    public void createBlog(String heading, String description) {
+        String query = "INSERT INTO blogs (heading, description) VALUES(?, ?)";
+        jdbcTemplate.update(query, heading, description);
     }
 }
