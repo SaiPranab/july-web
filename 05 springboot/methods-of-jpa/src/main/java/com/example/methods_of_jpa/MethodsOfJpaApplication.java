fa1f2395c2338ffc91caab462d18615c1050e2cc
Sai <saipranabpatra5233@gmail.com>
2025-11-10 23:28:30 +0530

    DT 10.11.25

--- 05 springboot/methods-of-jpa/src/main/java/com/example/methods_of_jpa/MethodsOfJpaApplication.java
+++ 05 springboot/methods-of-jpa/src/main/java/com/example/methods_of_jpa/MethodsOfJpaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 
@@ -38,7 +39,7 @@ public class MethodsOfJpaApplication {
 			// var savedProduct = productRepository.save(product);
 			// System.out.println("saved product is " + savedProduct);
 
-			// SAVE ALL
+			// // SAVE ALL
 			// var products = getProducts();
 			// productRepository.saveAll(products);
 
@@ -53,10 +54,12 @@ public class MethodsOfJpaApplication {
 			// System.out.println(".................... " +
 			// productRepository.existsById("f3c93d7d-ed9a-42c1-a541-1460726c89fd"));
 
+
 			// Delete
 			// productRepository.deleteById("f3c93d7d-ed9a-42c1-a541-1460726c89fd");
 
-			// FIND ALL (SELECT)
+			
+			// FIND ALL (SELECT) -> for extracting multiple rows
 			// var existingProducts = productRepository.findAll();
 			// existingProducts.forEach(System.out::println);
 			// existingProducts.forEach(p -> System.out.println(p));
@@ -64,9 +67,37 @@ public class MethodsOfJpaApplication {
 			// System.out.println(p);
 			// }
 
-			var sortedProductsByProductName = productRepository.findAll(Sort.by(Direction.DESC, "productName"));
-			sortedProductsByProductName.forEach(System.out::println);
+			// var sortedProductsByProductName = productRepository.findAll(Sort.by(Direction.DESC, "productName"));
+			// sortedProductsByProductName.forEach(System.out::println);
+
+			// var productsByPage = productRepository.findAll(PageRequest.of(0, 6));  // pageNumber (0 - based indexing), pageSize (number of data available in that page)
+			// var productsByPage = productRepository.findAll(PageRequest.of(0, 6, Sort.by(Direction.ASC, "productName")));
+			// productsByPage.forEach(System.out::println);
+
+
+			// FIND BY ID (SELECT) -> for extractin a single row 
+			// var existingProduct = productRepository.findById("f9fbe308-6d07-4aa3-a403-3e1f4dd9c593").orElseThrow(); // NoSuchElementException
+			// System.out.println("/////////////////////" + existingProduct);
+
+
+			// UPDATE
+			// var optProduct = productRepository.findById("f9fbe308-6d07-4aa3-a403");
+			// if(optProduct.isPresent()) {
+			// 	var existingProduct = optProduct.get();
+			// 	existingProduct.setProductPrice(existingProduct.getProductPrice() + 123);
+			// 	productRepository.save(existingProduct);
+			// } else {
+			// 	System.out.println("No Product Found");
+			// }
 		};
+
+
+		/**
+		 * Above emthods are already given by JPA Repository, but if we want to execute some custom SQL query
+		 * - Custom Query Methods (Query methods created by us)
+		 * - JPQL (Java Persistence Query Language)
+		 * - Native Query or Plain SQL
+		 */
 	}
 
 	private List<Product> getProducts() {
--- 05 springboot/methods-of-jpa/src/main/java/com/example/methods_of_jpa/model/Product.java
+++ 05 springboot/methods-of-jpa/src/main/java/com/example/methods_of_jpa/model/Product.java
@@ -29,5 +29,5 @@ public class Product {
 
   private String productBrand;
 
-  private double productPrice;
+  private Double productPrice;
 }
