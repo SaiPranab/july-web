1d66546f3a6c3c047a7cf8d09e0a9dfc03bced41
SaiPranab <saipranabpatra1234@gmail.com>
2025-11-12 21:09:02 +0530

    project completed

--- 05 springboot/jt-blogs/src/main/java/com/jt/jt_blogs/service/BlogService.java
+++ 05 springboot/jt-blogs/src/main/java/com/jt/jt_blogs/service/BlogService.java
@@ -1,11 +1,10 @@
 package com.jt.jt_blogs.service;
 
 import com.jt.jt_blogs.model.Blog;
+import com.jt.jt_blogs.repository.BlogRepository;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,44 +12,35 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class BlogService {
-    private final JdbcTemplate jdbcTemplate;
-    private static final String BLOGS_TABLE = "blogs";
+    private final BlogRepository repository;
 
     public List<Blog> getBlogs() {
-        String query = "SELECT * FROM %s".formatted(BLOGS_TABLE);
-        // List<Blog> existingBlogs = jdbcTemplate.query(query, (resultSet, rowNum) -> {
-        // int id = resultSet.getInt("id");
-        // String heading = resultSet.getString("heading");
-        // String description = resultSet.getString("description");
-
-        // return new Blog(id, heading, description);
-        // });
-
-        return jdbcTemplate.query(query, new BeanPropertyRowMapper<>(Blog.class));
+        List<Blog> existingBlogs = repository.findAll();
+        return existingBlogs;
     }
 
     public void createBlog(String heading, String description) {
-        String query = "INSERT INTO %s (heading, description) VALUES(?, ?)".formatted(BLOGS_TABLE);
-        jdbcTemplate.update(query, heading, description);
+        Blog newBlog = Blog.builder()
+                    .heading(heading)
+                    .description(description)
+                    .build();
+        repository.save(newBlog);
     }
 
-    public void deleteBlogById(String id) {
-        String query = "DELETE FROM %s WHERE id=?".formatted(BLOGS_TABLE);
-        jdbcTemplate.update(query, id);
+    public void deleteBlogById(int id) {
+        Blog toDeleteBlog = getBlogById(id);
+        repository.delete(toDeleteBlog);
     }
 
-    public Blog getBlogById(String id) {
-        String query = "SELECT * FROM %s WHERE id=?".formatted(BLOGS_TABLE);
-        return jdbcTemplate.queryForObject(query,
-                new BeanPropertyRowMapper<>(Blog.class), id);
+    public Blog getBlogById(int id) {
+        // Optional<Blog> optBlog = repository.findById(id);
+        // Blog blog = optBlog.orElseThrow();
+
+        Blog blog = repository.findById(id).orElseThrow();
+        return blog;
     }
 
     public void updateBlog(Blog blog) {
-        int id = blog.getId();
-        String heading = blog.getHeading();
-        String description = blog.getDescription();
-
-        String query = "UPDATE %s SET heading = ?, description = ? WHERE id = ?".formatted(BLOGS_TABLE);
-        jdbcTemplate.update(query, heading, description, id);
+        repository.save(blog);
     }
 }
