b6641dc9c6fe4b02bb5d6541e576a75a7a9fbda8
Avi-123222 <dasavishek669@gmail.com>
2025-11-30 17:00:47 +0530

    add of swagger and exception

--- 05 springboot/tasty-town/backend/src/main/java/com/tastytown/backend/controller/CategoryController.java
+++ 05 springboot/tasty-town/backend/src/main/java/com/tastytown/backend/controller/CategoryController.java
@@ -4,14 +4,11 @@ import java.util.List;
 
 // import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.tastytown.backend.model.Category;
 import com.tastytown.backend.service.ICategoryService;
@@ -22,9 +19,11 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/categories")
+@Tag(name = "Tasty-Town API",description = "This Controller manages CRUD operations for food order")
 public class CategoryController {
     private final ICategoryService service;
 
+    @Operation(summary="Get all food Category",description = "Fetch all food category")
     @GetMapping("/")
     public List<Category> getCategories() {
         return service.getCategories();
@@ -36,6 +35,9 @@ public class CategoryController {
     }
 
     @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary="Creates a new Food Category")
+    @ApiResponse(description = "Food Category validation failed",responseCode = "422")
     public Category addCategory(@RequestBody Category cat) {
         return service.addCategory(cat);
     }
@@ -46,6 +48,7 @@ public class CategoryController {
     }
 
     @DeleteMapping("/{catId}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public void deleteCategory(@PathVariable String catId) {
         service.deleteCategory(catId);
     }
---
+++ 05 springboot/tasty-town/backend/src/main/java/com/tastytown/backend/exception/HandleException.java
@@ -0,0 +1,76 @@
+package com.tastytown.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.lang.reflect.Field;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.StringJoiner;
+
+
+@RestControllerAdvice
+public class HandleException {
+    @ExceptionHandler(NoSuchElementException.class)
+    // @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ProblemDetail handleNoSuchException() {
+//        System.out.println("Resource not Found");
+//        Map<String,String> map = new HashMap<>();
+//        map.put("Title","Not Found");
+//        map.put("Detail","Resource not found");
+//        map.put("TimeStamp", LocalTime.now().toString());
+//        return map;
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Resource not found");
+        problemDetail.setProperty("timeStamp", LocalDateTime.now());
+        return problemDetail;
+    }
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ProblemDetail handleNoResourceFoundException(NoResourceFoundException e) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        problemDetail.setProperty("timeStamp", LocalDateTime.now());
+        return problemDetail;
+    }
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ProblemDetail handleStudentNotFoundException(CategoryNotFoundException e) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        problemDetail.setProperty("timeStamp", LocalDateTime.now());
+        return problemDetail;
+
+    }
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ProblemDetail handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.METHOD_NOT_ALLOWED, e.getMessage());
+        problemDetail.setProperty("timeStamp", LocalDateTime.now());
+        return problemDetail;
+
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValid(MethodArgumentNotValidException e){
+        StringJoiner joiner = new StringJoiner(",");
+        e.getAllErrors().forEach(error -> {
+            String message = error.getDefaultMessage();
+            String  fieldName = ((FieldError)error).getField() ;
+            joiner.add(fieldName+" "+ message);
+        });
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY,joiner.toString());
+        problemDetail.setProperty("timeStamp", LocalDateTime.now());
+        return problemDetail;
+
+    }
+}
